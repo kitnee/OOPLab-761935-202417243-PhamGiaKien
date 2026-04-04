f9e7809ee4337dd852dd72d972300ffc1a06a547
kitnee <kidlun2512@gmail.com>
2026-04-04 23:29:02 +0700

    Implement Cart and DigitalVideoDisc classes

---
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -0,0 +1,63 @@
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirectory() {
+        return director;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+        this.category = "";
+        this.director = "";
+        this.length = 0;
+        this.cost = 0.0f;
+    }
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = "";
+        this.length = 0;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = 0;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }   
+}
